8ecdb8c66329b46afd5bcf3067dc97d7e1d2106d
Marcelo Gumiero <mgumiero9@gmail.com>
2018-05-08 23:39:08 -0300

    Adds a simple list.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './components/List';
 
 class App extends Component {
   render() {
@@ -13,6 +14,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload. 
         </p>
+        <body className="Body">
+          <List />
+        </body>
       </div>
     );
   }
---
+++ src/components/ListItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class ListItem extends React.Component {
+    render() {
+        return (
+            <li>
+                <h4>
+                    {this.props.id}.
+                    {this.props.ingredient}
+                </h4>
+            </li>
+        );
+    }
+}
+
+export default ListItem;
