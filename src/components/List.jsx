c395b7aaffc76bef7743a628b9799adc546df297
Marcelo Gumiero <mgumiero9@gmail.com>
2018-05-09 10:26:11 -0300

    Improve formatting.

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,28 +1,25 @@
-import React from 'react';
-import ListItem from './ListItem';
+import React from "react";
+import ListItem from "./ListItem";
 
 var ingredients = [
-    { "id": 0, "text": "ham", "priority": "x" },
-    { "id": 1, "text": "cheese", "priority": "y" },
-    { "id": 2, "text": "potatoes", "priority": "z" }
+  { id: 0, text: "ham", priority: "x" },
+  { id: 1, text: "cheese", priority: "y" },
+  { id: 2, text: "potatoes", priority: "z" }
 ];
 
 class List extends React.Component {
-    render() {
-        var listItems = ingredients.map(
-            function (item) {
-                return <ListItem 
-                    id={item.id} 
-                    ingredient={item.text} 
-                    priority={item.priority} />
-            }
-        );
-        return (
-            <ul>
-                {listItems}
-            </ul>
-        );
-    }
+  render() {
+    var listItems = ingredients.map(function(item) {
+      return (
+        <ListItem
+          id={item.id}
+          ingredient={item.text}
+          priority={item.priority}
+        />
+      );
+    });
+    return <ul>{listItems}</ul>;
+  }
 }
 
 export default List;
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
 class ListItem extends React.Component {
-    render() {
-        return (
-            <li>
-                <h4>
-                    {this.props.id}.
-                    {this.props.ingredient}
-                </h4>
-            </li>
-        );
-    }
+  render() {
+    return (
+      <li>
+        <h4>
+          {this.props.id}.
+          {this.props.ingredient}
+        </h4>
+      </li>
+    );
+  }
 }
 
 export default ListItem;
