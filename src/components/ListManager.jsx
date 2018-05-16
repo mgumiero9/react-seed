5584ebef8a9e6b80ae735831e645ae62acb7f5e0
Marcelo Gumiero <mgumiero9@gmail.com>
2018-05-16 17:33:38 -0300

    Code Formatting

--- src/components/ListManager.jsx
+++ src/components/ListManager.jsx
@@ -4,29 +4,29 @@ import List from './List';
 class ListManager extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {items: [], newItemText:'' };
+        this.state = { items: [], newItemText: '' };
         this.currentItems = [];
-        
+
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(e) {
-        this.setState({newItemText: e.target.value});
+        this.setState({ newItemText: e.target.value });
     }
 
     handleSubmit(e) {
         e.preventDefault();
         this.currentItems.push(this.state.newItemText);
-        this.setState({items: this.currentItems, newItemText: ''});
+        this.setState({ items: this.currentItems, newItemText: '' });
     }
-    
-    render()  {
+
+    render() {
         return (
             <div>
                 <h3>{this.props.title}</h3>
                 <form onSubmit={this.handleSubmit}>
-                    <input type="text" onChange={this.handleChange} value={this.state.newItemText}/>
+                    <input type="text" onChange={this.handleChange} value={this.state.newItemText} />
                     <button type="submit">Add</button>
                 </form>
                 <List items={this.currentItems} />
